80568e90124cf957c8ef72575b2e027989344f55
clouding <ghost0436@gmail.com>
2017-05-13 01:21:05 +0800

    First commit.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'name'       => 'test',
+        'username'   => 'root',
+        'password'   => 'root',
+        'connection' => 'mysql:host=127.0.0.1',
+        'option'     => []
+    ]
+];
